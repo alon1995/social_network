92665730b68148b5f4ab55b448a62a5e8d08a816
alon <alon8600k@gmail.com>
2023-09-20 10:52:00 +0300

    first commit

---
+++ user/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, User
+from phonenumber_field.modelfields import PhoneNumberField
+
+# Create your models here.
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    phone_number = PhoneNumberField(unique=True)
+    profile_pitcure = models.ImageField()
+    about = models.TextField()
+    status = models.BooleanField(default=False)
+    birth_date = models.DateField()
+    is_staff = models.BooleanField(default=False)
+    last_login = User.last_login
+
+    USERNAME_FIELD = 'user_name'
+
+
+class UserPost():
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserFriend():
+    source_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    target_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    status = models.SmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    notes = models.CharField(max_length=500)
+
+
+class UserMessage():
+    source_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    target_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserFollower():
+    source_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    target_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
